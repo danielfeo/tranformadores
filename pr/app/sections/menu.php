16437df8cd26605c3152ed52ee61b18c98483387
eddcecc <cristhian.edic@gmail.com>
2016-09-03 17:38:38 -0500

    Menu experiencias

--- pr/app/sections/menu.php
+++ pr/app/sections/menu.php
@@ -1,6 +1,6 @@
 <?php
 $grupos = $app['mysql']->runQuery('SELECT * FROM grupos WHERE id_grupo = id_grupo_padre AND id_lenguaje = '.$app['lenguaje'].'')->getRows();
-echo '<ul class="secciones">';
+echo '<ul class="pasosInscripcion">';
     for ($i=0; $i<count($grupos); $i++)
-        echo '<li class="'.($app['pagina'] == $grupos[$i]['url'] ? 'active' : '').'" style="background-color:'.$grupos[$i]['color'].';"><a href="'.$grupos[$i]['url'].'">'.$grupos[$i]['titulo'].'</a></li>';
+        echo '<li class="it'.($i + 1).' '.($app['pagina'] == $grupos[$i]['url'] ? 'active' : '').'" style="background-color:'.$grupos[$i]['color'].';"><a href="'.$grupos[$i]['url'].'">'.$grupos[$i]['titulo'].'</a></li>';
 echo '</ul>';
