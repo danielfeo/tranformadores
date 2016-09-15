3576d94e8f020b201bf01b5e61d37cc7a6b1f4bf
daniel feo <danielandres.feo@gmail.com>
2016-09-14 21:21:01 -0500

    Merge branch 'master' of https://github.com/danielfeo/tranformadores
    # Conflicts:
    #	pr/app/base.php
    #	pr/public/css/app.css
    #	pr/public/js/fileupload.js

--- pr/mailserver.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$mailserver = array(
-    'host' => 'smtp.gmail.com',
-    'user' => 'web@redeamerica.org',
-    'pass' => 'giro2014',
-    'port' => '587',
-    'admin' => 'comunicaciones@redeamerica.org'
-); 
