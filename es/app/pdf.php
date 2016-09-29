27dc8deff9c79c3555b0a157cf02885e9445ae02
daniel feo <danielandres.feo@gmail.com>
2016-09-28 21:45:33 -0500

    v1.1

--- es/app/pdf.php
+++ es/app/pdf.php
@@ -10,7 +10,7 @@ $mpdf=new mPDF('utf-8', 'A4', 0, '', 15, 15, 30, 16, 9, 9, '');
 $header = '<div class="container">
         <div class="header">
             <div class="logo">
-                <img src="public/img/logo-transformadores.png" alt="">
+                <img width="142px" src="public/img/logo-transformadores.png" alt="">
             </div>
             <div class="tituloForm ColRojo">
                 <h1>Comunidades Sostenibles</h1>
@@ -18,23 +18,24 @@ $header = '<div class="container">
         </div>
         <div class="fecha">
             Agosto 26 de 2016
-        </div>';
+        </div><div class="main">';
 $stylesheet = file_get_contents('public/css/pdf.css');
 $mpdf->SetHTMLHeader($header);
-$html .= '<table width="100%" style="overflow: wrap">';
+//$html .= '<div class="main"><section><h2 class="ColNaranja">Datos generales de la Experiencia</h2>';
     foreach($grupos as &$grupo){
+        $html .= '<section>';
         if($grupo['url'] != 'InformacionAdicional'){
             if($grupo['id_grupo'] == $grupo['id_grupo_padre']){
-                $html .= '<tr><td colspan="2"><br></td></tr>';
-                $html .= '<tr><th colspan="2">'.str_replace('<br>', '', $grupo['titulo']).'</th></tr>';
+               $html .= '<h2 class="ColNaranja">'.str_replace('<br>', '', $grupo['titulo']).'</h2>';
             } else {
-                $html .= '<tr><th colspan="2" bgcolor="#aaa">'.str_replace('<br>', '', $grupo['titulo']).'</th></tr>';
+                $html .= '<div class="item"><h3>'.str_replace('<br>', '', $grupo['titulo']).'t1</h3></div>';
             }
             $preguntas = $app['mysql']->runQuery('SELECT p.pregunta, r.* FROM respuestas r, preguntas p, grupos g, experiencias e WHERE r.`id_pregunta` = p.`id_pregunta`  AND r.`id_experiencia` = e.`id_experiencia` AND p.`id_grupo` = g.`id_grupo`  AND YEAR(e.`inicio`) = "'.$app['pdf_fecha'].'" AND g.`id_grupo_padre` = '.$grupo['id_grupo'].' AND r.`id_usuario` = '.$app['pdf_user'].' order by r.id_pregunta')->getRows();
             for ( $i=0; $i<count($preguntas); $i++ ){
-                $html .= '<tr><td width="35%">'.$preguntas[$i]['pregunta'].'</td><td>'.$preguntas[$i]['respuesta'].'</td></tr>';
+                $html .= '<div class="item"><h3>'.$preguntas[$i]['pregunta'].'</h3><p> '.$preguntas[$i]['respuesta'].'</p></div>';
             }
         }
+        $html .= '</section>';
     }
 
     $html .= '<tr><td colspan="2"><br></td></tr>';
@@ -45,10 +46,8 @@ $html .= '<table width="100%" style="overflow: wrap">';
             $filename = explode('/', $archivos[$a]);
             $html .= '<a href="'.$archivos[$a].'" target="_blank">'.mb_strtolower(end($filename), 'UTF-8').'</a><br>';
         }
-        $html .= '</td>';
-    $html .= '</tr>';
-$html .= '</table>';
-
+       $html .= '</div></div>';
+    
 $mpdf->WriteHTML($stylesheet,1);
 
 $mpdf->WriteHTML($html, 2);
